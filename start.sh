7a8f04134b90b6d8016ab7b899a0584a818dcf1b
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-09-11 20:41:37 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,29 @@
+Crontab_file="/usr/bin/crontab"
+Green_font_prefix="\033[32m"
+Red_font_prefix="\033[31m"
+Green_background_prefix="\033[42;37m"
+Red_background_prefix="\033[41;37m"
+Font_color_suffix="\033[0m"
+Info="[${Green_font_prefix}信息${Font_color_suffix}]"
+Error="[${Red_font_prefix}错误${Font_color_suffix}]"
+Tip="[${Green_font_prefix}注意${Font_color_suffix}]"
+
+check_root() {
+    [[ $EUID != 0 ]] && echo -e "${Error} 当前非ROOT账号(或没有ROOT权限), 无法继续操作, 请更换ROOT账号或使用 ${Green_background_prefix}sudo su${Font_color_suffix} 命令获取临时ROOT权限（执行后可能会提示输入当前账号的密码）。" && exit 1
+}
+
+install_env_and_full_node() {
+    check_root
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu unzip zip docker.io -y
+    VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+    DESTINATION=/usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+    sudo chmod 755 $DESTINATION
+
+    sudo apt-get install npm -y
+    sudo npm install n -g
+    sudo n stable
+    sudo npm i -g yarn
+}
+install_env_and_full_node;
